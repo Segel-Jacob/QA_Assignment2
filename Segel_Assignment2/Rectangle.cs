ece9152a81ccbf0b5686dbc8a46e3cebbd3a3abf
Segel Jacob <svarapathukuzhi5556@conestogac.on.ca>
2021-02-16 19:33:52 -0500

    contains complete rectangle class

---
+++ Segel_Assignment2/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segel_Assignment2
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        //default contructor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        //non-default constructor
+        public Rectangle(int l, int w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public int GetLenght()
+        {
+            return length;
+        }
+
+        public int SetLength(int l)
+        {
+            length = l;
+            return length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int w)
+        {
+            width = w;
+            return width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
+
